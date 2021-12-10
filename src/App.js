25aa3241fba0d04264ac1ca274ca303db59ea03f
pavlo-lytvynenko <pavlo.lytvynenko@agiliway.com>
2021-12-10 09:44:40 +0200

    ref - Cotrols. add - disabled control buttons

--- src/App.js
+++ src/App.js
@@ -8,34 +8,11 @@ import Controls from './components/Controls';
 // |--- COMPONENT
 function App() {
   const [todos, setTodos] = useState(dataTodo); 
-  const controls = [
-    { name: 'add item', className: classes.button, handler: handleAddTodo },
-    { name: 'remove item', className: classes.button, handler: handleRemoveTodo }
-  ];
-
-  function handleAddTodo() {
-    const updatedTodos = [...todos];
-
-    updatedTodos.push({
-      id: updatedTodos.length,
-      name: ''
-    });
-
-    setTodos( updatedTodos );
-  }
-
-  function handleRemoveTodo() {
-    const updatedTodos = [...todos];
-
-    updatedTodos.pop();
-
-    setTodos( updatedTodos );
-  }
 
   return (
     <div className={classes.container}>
       <List todos={todos}/>
-      <Controls controls={controls}/>
+      <Controls todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
--- src/components/Controls/index.js
+++ src/components/Controls/index.js
@@ -1,12 +1,59 @@
 import React from 'react';
 import classes from '../../App.module.css';
 
-const Controls = ({controls})=>{
+const Controls = ({todos, setTodos}) => {
+  const controls = [
+    {
+      name: 'add item',
+      className: classes.button,
+      handler: handleAddTodo,
+      active: true,
+      isDisabled: todos.length===10? true: false,
+    },
+    {
+      name: 'remove item',
+      className: classes.button,
+      handler: handleRemoveTodo,
+      active: todos.length? true: false,
+      isDisabled: !todos.length? true: false,
+    }
+  ];
+
+  // |--- Handlers
+  function handleAddTodo() {
+    const updatedTodos = [...todos];
+
+    updatedTodos.push({
+      id: updatedTodos.length+1,
+      name: ''
+    });
+
+    setTodos( updatedTodos );
+  }
+
+  function handleRemoveTodo() {
+    const updatedTodos = [...todos];
+
+    updatedTodos.pop();
+
+    setTodos( updatedTodos );
+  }
+
+  // |--- Render
   return (
     <div className={classes.todoControls}>
-      { controls.map(({name, className, handler}) =>
-          <button className={className} onClick={handler}>{name}</button>
-      )}
+      { controls.map(({name, className, handler, isDisabled}) => {
+        return (
+          <button
+            key={name}
+            className={className}
+            onClick={handler}
+            disabled={isDisabled}
+          >
+            {name}
+          </button>
+        )
+      })}
     </div>
   );
 }
